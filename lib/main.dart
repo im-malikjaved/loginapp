fa63e56263be7e7c5084e45e5223bb03c23c8e39
im-malikjaved <muhammadjaved444@gmail.com>
2022-05-16 21:34:46 +0500

    ok ok

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Login Panel'),
     );
   }
 }
@@ -35,13 +35,83 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/bg.jpeg"), fit: BoxFit.cover),
-        ),
-        child: const Text("hello mya app"),
-      ),
+          constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg.jpeg"), fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Container(
+              width: 300,
+              height: 400,
+              color: const Color.fromARGB(240, 46, 38, 38).withOpacity(0.3),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 70,
+                      child: Text(
+                        "Login Panel",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 30),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextField(
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w300),
+                        decoration: InputDecoration(
+                            hintText: "Username",
+                            hintStyle: TextStyle(color: Colors.white60)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.white60)),
+                        obscureText: true,
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox.fromSize(
+                      size: const Size(56, 56), // button width and height
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.lightBlue, // button color
+                          child: InkWell(
+                            splashColor: Colors.white, // splash color
+                            onTap: () {}, // button pressed
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.arrow_forward_ios), // icon
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
